901c54e97cb390868ac8756790d6cc49243180bf
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-09-02 18:16:44 +0200

    Roman_To_Int

--- Problems/13_Roman_to_Integer/Solution.java
+++ Problems/13_Roman_to_Integer/Solution.java
@@ -2,12 +2,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    public static void main(String[] args) {
-        int numero = romanToInt_01("MCMXCIV");
+    public static void main(String[] args) { 
+        int numero = romanToInt_05("MCMXCIV");
         System.out.println(numero);
-    }
+    } // Primera variante donde donde guardamos los datos con put en el HashMap
 
-    // Primera variante donde donde guardamos los datos con put en el HashMap
+    // Estos primeros tres métodos vamos a trabajar con mapas
     public static int romanToInt_01(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
@@ -81,6 +81,90 @@ public class Solution {
                 numero += map.get(s.charAt(i));
             }
         }
-        return map.get(s.charAt(s.length() - 1));
+        return numero + map.get(s.charAt(s.length() - 1));
+    }
+
+    // Los siguientes métodos vamos a trabajar con switches
+    public static int romanToInt_04(String s) {
+        int resultado = 0;
+        int numero = 0;
+
+        // Recorremos el string de derecha a izquierda
+        for (int i = s.length() - 1; i >= 0; i++) {
+            // En lugar de usar un Map para establecer el valor de cada letra del número romano usamos un switch
+            // Este valor lo almacenamos en una variable temporal que luego se sumará o restará al resultado final
+            switch (s.charAt(i)) {
+                case 'I':
+                    numero = 1;
+                    break;
+                case 'V':
+                    numero = 5;
+                    break;
+                case 'X':
+                    numero = 10;
+                    break;
+                case 'L':
+                    numero = 50;
+                    break;
+                case 'C':
+                    numero = 100;
+                    break;
+                case 'D':
+                    numero = 500;
+                    break;
+                case 'M':
+                    numero = 1000;
+                    break;
+                default:
+                    break;
+            }
+
+            // El * 4 es un truco para saber si un símbolo romano se resta en lugar de sumarse
+            // En los números romanos, solo se resta cuando el simbolo es mucho más pequeño que el que viene después (como 'I' antes de 'V' o 'X' antes de 'L' o C)
+            // Por lo tanto esa regla siempre se cumple cuando el número actual es menor que la cuarta parte del valor que llevamos en 'resultado'
+
+            // Si número * 4 es muy pequeño en comparación con lo que ya sumamos -> resta
+            // Si no -> suma
+            if (4 * numero < resultado) {
+                resultado -= numero; 
+            } else {
+                resultado += numero;
+            }
+        }
+
+        return resultado;
+    }
+
+    public static int romanToInt_05(String s) {
+        int resultado = 0; // Variable que vamos a devolver
+
+        // Bucle con el cual recorremos la frase
+        for (int i = 0; i < s.length(); i++)  {
+            // Conseguimos el valor del número actual
+            int valorActual = getValue(s.charAt(i));
+
+            // Si i es menor que el tamaño de la frase - 1 Y el valor actual es menor que el siguiente (I < V) (1 < 5)
+            if (i < s.length() - 1 && valorActual < getValue(s.charAt(i + 1))) {
+                resultado -= valorActual; 
+            } else {
+                resultado += valorActual;
+            }
+        }
+
+        return resultado;
+    }
+
+    // Método con el cual retornamos el valor del número romano
+    public static int getValue(char c) {
+        switch (c) {
+            case 'I': return 1; 
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
